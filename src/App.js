c3ebfa323e3926210e7607af394f4d58f700592c
Ihor kulpekin <konstantin.boykov@pro-store.online>
2021-01-23 22:18:33 +0200

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import {Route, Switch} from 'react-router-dom';
 
 import Header from './components/Header/Header';
-import ConverterBlock from './components/converter/ConverterBlock';
-import CryptocurrenciesContainer from './components/CryptocurrenciesContainer';
-import DetailsCryptocurrencyContainer from './components/DetailsCryptocurrency/DetailsCryptocurrencyContainer';
+
+const DetailsCryptocurrencyContainer = lazy(() => import('./components/DetailsCryptocurrency/DetailsCryptocurrencyContainer'))
+const ConverterBlock = lazy(()=> import('./components/converter/ConverterBlock'))
+const CryptocurrenciesContainer = lazy(()=> import('./components/CryptocurrenciesContainer'))
 
 const App = () => {
-  return (
-    <>
-      <Header/>
-      <Switch>
-        <Route exact path="/">
-          <ConverterBlock/>
-          <CryptocurrenciesContainer/>
-        </Route>
-        <Route path="/cryptocurrencies/:id" component={DetailsCryptocurrencyContainer}/>
-      </Switch>
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <Suspense fallback={<div>Page is Loading...</div>}>
+                <Switch>
+                    <Route exact path="/">
+                        <ConverterBlock/>
+                        <CryptocurrenciesContainer/>
+                    </Route>
+                    <Route path="/cryptocurrencies/:id" component={DetailsCryptocurrencyContainer}/>
+                </Switch>
+            </Suspense>
+        </>
+    );
 };
 
 export default App;
